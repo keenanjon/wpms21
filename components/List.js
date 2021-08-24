fc3e3187b06a64df3963d723515a706953599ff1
keenanjon <jon.menna@metropolia.fi>
2021-08-24 10:40:06 +0300

    effect, try, etc

--- components/List.js
+++ components/List.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
 
 const List = (props) => {
+  const [mediaArray, setMediaArray] = useState([]);
   const url =
     'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
-  const mediaArray = [];
-  const loadMedia = async () => {
-    const response = await fetch(url);
-    const json = await response.json();
-    console.log(json);
-  };
 
-  loadMedia();
+  useEffect(() => {
+    const loadMedia = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setMediaArray(json);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    loadMedia();
+  }, []);
+
+  console.log('List rivi 23', mediaArray);
   return (
     <FlatList
       data={mediaArray}
       renderItem={({item}) => <ListItem singleMedia={item} />}
+      keyExtractor={(item, index) => index.toString()}
     />
   );
 };
